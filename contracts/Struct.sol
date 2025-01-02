6ec8e575b981cf8a518adeec4a30af5b80408b74
gaurisonawane07 <gaurisonawane918@gmail.com>
2025-01-02 17:54:01 +0530

    smart wallet

--- contracts/Struct.sol
+++ contracts/Struct.sol
@@ -1,2 +1,23 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.26;
+
+
+
+contract wallet {
+
+    PaymentReceived public payment;
+    
+
+    function payContract() public payable {
+        payment = new PaymentReceived(msg.sender,msg.value);
+    }
+}
+contract PaymentReceived {
+    address public from;
+    uint public amount;
+
+    constructor(address _from,uint _amount) payable {
+        from = _from;
+        amount = _amount;
+    }
+}
